038effd5e2725bfa1aaea8386e3a05451077d9e3
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-10-13 21:47:42 +0300

    creating a bridge between the backend and arduino

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,31 @@
-import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 import dotenv from 'dotenv';
+import express from 'express';
 import sequelize from './config/sequelize'; 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const url = process.env.ARDUINO_URL || '';
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/sensors', (req, res) => {
-  const sensorData = [
-    { temperature: 22, humidity: 60 },
-    { temperature: 23, humidity: 55 },
-  ];
-  res.status(200).json(sensorData);
-});
+axios.get(url)
+  .then(response => {
+    console.log('Data:', response.data);
+    app.get('/api/sensors', (req, res) => {
+    const sensorData = [response.data];
+    res.status(200).json(sensorData);
+  });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 
+  
 sequelize.authenticate()
   .then(() => {
     console.log('Connected to PostgreSQL database successfully!');
