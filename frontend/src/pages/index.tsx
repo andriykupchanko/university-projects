f47915bc19179275be355bf77961612697d0edf3
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-10-27 21:54:26 +0200

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,22 @@
-// pages/index.tsx
 import React from "react";
+import "tailwindcss/tailwind.css"; // імпорт Tailwind CSS
 import Dashboard from "@/components/layout/Dashboard";
+import AuthButton from "@/components/common/AuthButton";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to My Sensor Data Dashboard</h1>
-      <Dashboard isAuthenticated={false} />
+    <div className="">
+      <h1 className="text-3xl font-extrabold text-center p-7">
+        Welcome to My Sensor Data Dashboard
+      </h1>
+      <div className="flex w-screen  h-screen">
+        <div className="w-[70%] p-4">
+          <Dashboard isAuthenticated={true} />
+        </div>
+        <div className="w-[30%] flex mt-12 justify-center ">
+          <AuthButton />
+        </div>
+      </div>
     </div>
   );
 };
